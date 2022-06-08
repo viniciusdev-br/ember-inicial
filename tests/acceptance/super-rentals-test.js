35e222a85351b63038c36bab51681545add75c3f
Vinicius Botelho <viniciusvcb2002@gmail.com>
2022-06-08 08:37:38 -0300

    A partir deste commit, será refatorado para aversão 2.4

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -27,4 +27,22 @@ module('Acceptance | super rentals', function (hooks) {
 
     assert.strictEqual(currentURL(), '/contact');
   });
+
+  test('navigating using the nav-bar', async function (assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.strictEqual(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.strictEqual(currentURL(), '/contact');
+
+    await click('nav a.menu-index');
+    assert.strictEqual(currentURL(), '/');
+  });
 });
